a70446272ba4bcfc0940a0c63e2d88de87c25c81
seval2019 <sevalsenturk2017@gmail.com>
2023-10-31 01:15:48 +0300

    versiyon3(tailwind and font ayarları)

---
+++ pages/home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className="text-red-600 font-dancing text-center text-3xl">Psychology</div>
+  )
+}
+
+export default index
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        primary: "#00bcd4",
+        primary: "#ffbe33",
+        secondary: "#222831",
+      }, 
+      fontFamily: {
+        oswald: ["Oswald, sans-serif"], // Adds a new `font-display` class,
+        dancing: ["Dancing Script", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
